8f0fb2f4ad7d8534a44a658ec9bd1f9cb2cccb06
houman yousefiani <hmnusf@gmail.com>
2020-01-24 00:59:04 +0330

    income in models and beautification!

--- app1/models.py
+++ app1/models.py
@@ -6,3 +6,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True)
+    def __str__(self):
+        return "{} - {} - {}".format(self.date,self.amount,self.text)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True)
+    def __str__(self):
+        return "{} - {} - {}".format(self.date,self.amount,self.text)
